5ef97af1b262e1b8fce9b16e0c343b58dbc21c6e
ikennaex <ikennaexcel@gmail.com>
2026-03-03 10:58:29 +0100

    fix: buyData

--- routes/VTU/Data/dataRoutes.js
+++ routes/VTU/Data/dataRoutes.js
@@ -2,7 +2,7 @@ const express = require('express')
 const { getDataPlans, getDataNetworks, buyData } = require('../../../controllers/VTU/Data/dataController')
 const router = express.Router()
 
-router.get('/get-data-networks', getDataNetworks )    
-router.get('/get-data-plans', getDataPlans )    
-router.post('/buy-data', buycData )    
+router.get('/get-data-networks', getDataNetworks )  
+router.get('/get-data-plans', getDataPlans )
+router.post('/buy-data', buyData )
 module.exports = router  
