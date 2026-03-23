59b0e3b04bb8e6df583a75408f82081fad17f58e
ikennaex <ikennaexcel@gmail.com>
2026-03-23 05:12:25 +0100

    update: verifyPasscode on smm

--- routes/SMM/smmRoute.js
+++ routes/SMM/smmRoute.js
@@ -1,10 +1,11 @@
 const express = require('express')
 const { getLikesServices, getFollowersServices, buyLikes, buyFollowers } = require('../../controllers/SMM/smmController')
 const { authToken } = require('../../middleware/auth')
+const verifyPasscode = require('../../middleware/verifyPasscode')
 const router = express.Router()
 
 router.get('/likes', getLikesServices)   
 router.get('/followers', getFollowersServices)  
-router.post('/buy-likes', authToken, buyLikes)  
-router.post('/buy-followers', authToken, buyFollowers)  
+router.post('/buy-likes', authToken, verifyPasscode, buyLikes)  
+router.post('/buy-followers', authToken, verifyPasscode, buyFollowers)  
 module.exports = router  
