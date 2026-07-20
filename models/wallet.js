eeac1cf97fc01ad00d013872052c7bceeaa35ad3
ikennaex <ikennaexcel@gmail.com>
2026-07-20 16:06:06 +0100

    update: fix wallet

--- models/wallet.js
+++ models/wallet.js
@@ -1,12 +1,19 @@
 const mongoose = require("mongoose");
 const { Schema } = mongoose;
 
-const walletSchema = new Schema({
-  user: { type: mongoose.Schema.Types.ObjectId, ref: "User" },
-  balance: { type: Number, default: 0 },
-  isFrozen: { type: Boolean, default: false },
-}, { timestamps: true });
-
+const walletSchema = new Schema(
+  {
+    user: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+      unique: true,
+    },
+    balance: { type: Number, default: 0 },
+    isFrozen: { type: Boolean, default: false },
+  },
+  { timestamps: true },
+);
 
 const walletModel = mongoose.model("Wallet", walletSchema);
 module.exports = walletModel;
